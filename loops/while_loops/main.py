9d3b02bf8b48d77558b1ad4401740f049126824d
tjhangowa <>
2026-02-19 22:27:10 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,9 @@
+start_number = 5
+countdown_values = []
+
+while start_number >= 1:
+    countdown_values.append(start_number)
+    start_number-=1
+
+print(f"Discount countdown complete! {countdown_values}")
+    
